2cd5614f5e4041d8212d63e1f1c171d078a150a2
Patrick May <patrickpm.may@gmail.com>
2021-07-01 14:22:34 -0500

    bootstrapify new, index pages

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
 const cities = require("./cities");
-const {places, descriptors} = require("./seedhelpers")
+const { places, descriptors } = require("./seedhelpers");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -15,20 +15,25 @@ db.once("open", () => {
   console.log("Database Connected");
 });
 
-const sample = (array) => array[Math.floor(Math.random()*array.length)]
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
-    const camp =  new Campground({
+    const price = Math.floor(Math.random() * 30 + 10);
+    const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime expedita consequatur atque, doloremque praesentium soluta quia et? Provident alias quae minus ipsam praesentium quas sequi vitae nesciunt, nisi, doloribus porro?",
+      price,
     });
-    await camp.save()
+    await camp.save();
   }
 };
 
-seedDB().then(()=>{
-    mongoose.connection.close()
+seedDB().then(() => {
+  mongoose.connection.close();
 });
