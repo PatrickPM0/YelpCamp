c6ac330ec7e9ae66ff99557aace5997a11c90356
Patrick May <patrickpm.may@gmail.com>
2021-07-08 21:19:33 -0500

    create flash succes and error

--- routes/camgrounds.js
+++ routes/camgrounds.js
@@ -44,6 +44,10 @@ router.get(
     const campground = await Campground.findById(req.params.id).populate(
       "reviews"
     );
+    if(!campground){
+      req.flash("error", "That campground does not exist.")
+      return res.redirect("/campgrounds")
+    }
     res.render("campgrounds/show", { campground });
   })
 );
@@ -64,6 +68,7 @@ router.put(
     const campground = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     });
+    req.flash("success", "Successfully updated campground!")
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
@@ -73,6 +78,7 @@ router.delete(
   catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash("success", "Successfully deleted campground.")
     res.redirect("/campgrounds");
   })
 );
