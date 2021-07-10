e92a8ae42497900dde69ab7c7a1a654cca6e0925
Patrick May <patrickpm.may@gmail.com>
2021-07-09 19:03:54 -0500

    create returnTo behavior

--- routes/users.js
+++ routes/users.js
@@ -39,7 +39,8 @@ router.post(
   }),
   (req, res) => {
     req.flash("success", "Welcome back!");
-    res.redirect("/campgrounds");
+    const redirectUrl = req.session.returnTo || "/campgrounds";
+    res.redirect(redirectUrl);
   }
 );
 
